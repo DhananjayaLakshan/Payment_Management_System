4b1831acba45bf28692aed2058cf6ce986b2d619
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2023-12-06 20:36:53 +0530

    add validations to sign up process

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,28 +14,43 @@ const usersSchema = new Schema({
 // static signup method
 usersSchema.statics.signup = async function(userName, email, password) {
     try {
-        const existsEmail = await this.findOne({ email });
+
+        //validator
+        if (!userName || !email || !password) {
+            throw Error('All fields must be filled')
+        }
+
+        if (!validator.isEmail(email)) {
+            throw Error('Email is not valid')
+        }
+
+        if (!validator.isStrongPassword(password)) {
+            throw Error('Password not string enough')
+        }
+
+        const existsEmail = await this.findOne({ email })
 
         if (existsEmail) {
-            throw new Error('Email already in use');
+            throw new Error('Email already in use')
         }
 
-        const existsUsername = await this.findOne({ userName });
+        const existsUsername = await this.findOne({ userName })
 
         if (existsUsername) {
-            throw new Error('Username already in use');
+            throw new Error('Username already in use')
         }
 
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
 
-        const user = await this.create({ userName, email, password: hash });
+        const user = await this.create({ userName, email, password: hash })
 
-        return user;
+        return user
+
     } catch (error) {
-        throw error;
+        throw error
     }
-};
+}
 
 
 module.exports = mongoose.model('Users', usersSchema)
