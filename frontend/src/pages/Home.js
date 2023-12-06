4ebef93742b71e68c3318d1326d6c6a8d4f5872e
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2023-12-06 13:57:58 +0530

    add a table

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,8 +4,72 @@ import Navbar from '../components/Navbar'
 export default function Home() {
     return (
         <div>
-           
-            Home
+            <h1>Home</h1>
+
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Admin Account</th>
+                        <th scope="col">Payment</th>
+                        <th scope="col">paid</th>
+                        <th scope="col">pending</th>
+                        <th scope="col">Mobile Number</th>
+                        <th scope="col">Starting Date</th>
+                        <th scope="col">Due Date</th>
+                        <th scope="col">Duration</th>
+                        <th scope="col">Group</th>
+                        <th scope="col">Payment Update</th>
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
