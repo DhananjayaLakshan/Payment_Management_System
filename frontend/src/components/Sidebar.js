a259462065423a301190145c47a2e434f34bed9b
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2023-12-24 22:19:14 +0530

    set categories to sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,37 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext';
+import { AiFillCaretRight } from "react-icons/ai";
+
 
 export default function Sidebar() {
+    // Retrieve dispatch function from the workouts context
+    const { dispatch } = useWorkoutsContext()
+    // Retrieve user information from the authentication context
+    const { user } = useAuthContext()
+
+    //fetch Data
+    const [workouts, setWorkouts] = useState([])
+
+    //fetch
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
+
+            const json = await response.json()
+
+            setWorkouts(json)
+            
+
+            if (response.ok) {
+                dispatch({ type: 'SET_WORKOUT', payload: json })
+            }
+        }
+
+        if (user) {
+            fetchWorkouts()
+        }
+
+    }, [dispatch, user])
+
+    // Define a function to get unique categories
+    const getUniqueCategories = () => {
+        const uniqueCategories = [...new Set(workouts.map((workout) => workout.category))];
+        return uniqueCategories;
+    };
+
     return (
         <div className='bg-white  p-2'>
 
             <div className='m-2'>
                 <i className='bi bi-bootstrap-fill me-2 fs-4'></i>
-                <span className='brand-name fs-4'>Yoursaf</span>
+                <span className='brand-name fs-4'>CATEGORIES</span>
             </div>
 
             <hr  className='text-dark'/>
 
             <div className='list-group list-group-flush'>
 
+            {getUniqueCategories().map((uniqueCategory) => (
+                                
                 <a className='list-group-item  list-group-item-action py-2'>
-                    <i className='bi bi-house fs-5 me-3'></i>
-                    <span className='fs-5'>Home</span>
+                    <i className='bi bi-house fs-5 me-3'><AiFillCaretRight /></i>
+                    <span className='fs-5'>{uniqueCategory}</span>
                 </a>
-                
-                <a className='list-group-item  list-group-item-action py-2'>
-                    <i className='bi bi-house fs-5 me-3'></i>
-                    <span className='fs-5'>Home</span>
-                </a>
-
-                <a className='list-group-item list-group-item-action py-2'>
-                    <i className='speedometer2 fs-4 me-3'></i>
-                    <span className='fs-5'>Customer</span>
-                </a>
-                
-                <a className='list-group-item list-group-item-action py-2'>
-                    <i className='speedometer2 fs-4 me-3'></i>
-                    <span className='fs-5'>payment</span>
-                </a>
+            ))}
 
             </div>
         </div>
